a167f7ad0a1b0a12391f92569c84e9e5d7fd51eb
David Bonanza <bonanzadavid@gmail.com>
2021-04-03 17:28:55 -0400

    Update TurnToAngle.java
    Add comments referencing two sources for TurnToAngle

--- 2020-Flight-Code-Imported/src/main/java/frc/robot/commands/chassis/TurnToAngle.java
+++ 2020-Flight-Code-Imported/src/main/java/frc/robot/commands/chassis/TurnToAngle.java
@@ -1,4 +1,2 @@
-// Turning to a set heading
-// https://docs.wpilib.org/en/stable/docs/software/sensors/gyros-software.html#turning-to-a-set-heading
-// Note
-// Turn-to-angle loops can be tricky to tune correctly due to static friction in the drivetrain, especially if a simple P loop is used. There are a number of ways to account for this; one of the most common/effective is to add a “minimum output” to the output of the control loop.
+// https://pdocs.kauailabs.com/navx-mxp/examples/rotate-to-angle-2/#:~:text=Automatically%20rotating%20a%20robot%20to,spin%20on%20a%20dime%E2%80%9D).
+// https://pdocs.kauailabs.com/navx-mxp/examples/rotate-to-angle-2/#:~:text=Automatically%20rotating%20a%20robot%20to,spin%20on%20a%20dime%E2%80%9D).
